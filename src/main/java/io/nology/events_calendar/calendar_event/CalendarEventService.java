c88576e48e49eca735ffcda5b23427406fb85d4f
Murielle Beaulieu <muriellebeaulieu@Murielles-MacBook-Pro.local>
2025-05-01 12:09:40 +1000

    wip - display event to the calendar

--- src/main/java/io/nology/events_calendar/calendar_event/CalendarEventService.java
+++ src/main/java/io/nology/events_calendar/calendar_event/CalendarEventService.java
@@ -3,7 +3,6 @@ package io.nology.events_calendar.calendar_event;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
